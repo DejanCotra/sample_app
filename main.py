65ab2be44cadfe7575f7b847437415f833c774ba
Dejan Cotra <dejancotra@gmail.com>
2021-02-11 14:11:41 +0100

    change print

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import socket
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65431        # Port to listen on (non-privileged ports are > 1023)
 
-print ("Hello world!!!")
+print (">>> Hello world <<<")
 
 # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 #     s.bind((HOST, PORT))
