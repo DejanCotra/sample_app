c608e2df04494790b997f5c9eff6ae6e3ea03127
Dejan Cotra <dejancotra@gmail.com>
2021-02-12 13:45:16 +0100

    inifinite loop

--- main.py
+++ main.py
@@ -3,21 +3,21 @@
 import socket
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 65431        # Port to listen on (non-privileged ports are > 1023)
+PORT = 8080         # Port to listen on (non-privileged ports are > 1023)
 
 print (">>> Hello world <<<")
 
-while 1:
-    pass
+""" while 1:
+    pass """
 
-# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-#     s.bind((HOST, PORT))
-#     s.listen()
-#     conn, addr = s.accept()
-#     with conn:
-#         print('Connected by', addr)
-#         while True:
-#             data = conn.recv(1024)
-#             if not data:
-#                 break
-#             conn.sendall(data)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall("Test")
