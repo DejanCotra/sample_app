3c90ea81b007943fa17092f799950c57efda7b95
Dejan Cotra <dejancotra@gmail.com>
2021-02-12 15:19:19 +0100

    inifinite loop

--- main.py
+++ main.py
@@ -17,10 +17,11 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     with conn:
         print('Connected by', addr)
         while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+            # data = conn.recv(1024)
+            # if not data:
+            #     break
+            #conn.sendall()
+            conn.sendall(b'Hello, world')
 
 print (">>> End <<<")
 while 1:
